ed4718e942c7407e8779946c430afd40fcfdc074
David14gb <davidgarciab14@gmail.com>
2022-05-31 11:06:29 +0200

    dia2

--- proyecto_node/dia2ThenCatch/reto2.js
+++ proyecto_node/dia2ThenCatch/reto2.js
@@ -1,4 +1,17 @@
-
+const readline = require("readline")
+function pregunta(pregunta){
+    const question = new Promise((resolve, reject) => {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+        rl.question(pregunta, (respuesta) => {
+            resolve(respuesta);
+            rl.close();
+        });
+    });
+    return question
+}
 
 let person = {
     "name": "David",
